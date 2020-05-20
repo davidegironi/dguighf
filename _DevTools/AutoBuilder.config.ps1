a7a485fa490d1f2efdc223e30468b93d260832f0
Davide Gironi <davide.gironi@gmail.com>
2020-05-20 16:26:09 +0200

    updated nuspec dependencies, updated version

--- _DevTools/AutoBuilder.config.ps1
+++ _DevTools/AutoBuilder.config.ps1
@@ -6,7 +6,7 @@ $solutionName = "DGUIGHF"
 $versionMajor = "1"
 $versionMinor = "2"
 $versionBuild = GetVersionBuild
-$versionRevision = "13"
+$versionRevision = "14"
 #build version number
 $assemblyVersion = GetVersion $versionMajor $versionMinor $versionBuild $versionRevision
 $fileVersion = $assemblyVersion
